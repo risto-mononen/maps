b2819f9cc2241083e13147dcab0912c959a2b878
Risto Mononen <risto.mononen@gmail.com>
2015-11-09 23:08:06 +0200

    Back to real input, comment out test.atom

--- UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
+++ UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
@@ -77,7 +77,7 @@ public class EarthquakeCityMap extends PApplet {
 		
 		// FOR TESTING: Set earthquakesURL to be one of the testing files by uncommenting
 		// one of the lines below.  This will work whether you are online or offline
-		earthquakesURL = "test1.atom";
+		//earthquakesURL = "test1.atom";
 		//earthquakesURL = "test2.atom";
 		
 		// WHEN TAKING THIS QUIZ: Uncomment the next line
