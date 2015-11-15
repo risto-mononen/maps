350e47e666b95497f49ba26b3ce89ec461362d49
Risto Mononen <risto.mononen@gmail.com>
2015-11-15 13:38:22 +0200

    Show title for a city

--- UCSDUnfoldingMaps/src/module5/CityMarker.java
+++ UCSDUnfoldingMaps/src/module5/CityMarker.java
@@ -50,8 +50,10 @@ public class CityMarker extends CommonMarker {
 	/** Show the title of the city if this marker is selected */
 	public void showTitle(PGraphics pg, float x, float y)
 	{
-		
-		// TODO: Implement this method
+			String title = getCity() + "\n" + getCountry() + "\n" + getPopulation();
+			System.err.println(title + "\t" + pg + "\t" + x + "\t" + y);
+			pg.fill(0);
+			pg.text(title, x, y);
 	}
 	
 	
