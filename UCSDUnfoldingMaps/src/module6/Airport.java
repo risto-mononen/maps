fba7b613964839e2c389fe80d4a2eee64de10ee1
Risto Mononen <risto.mononen@gmail.com>
2015-11-26 20:28:00 +0200

    Draw airports on mouseclick, in random order (or id order?)

--- UCSDUnfoldingMaps/src/module6/Airport.java
+++ UCSDUnfoldingMaps/src/module6/Airport.java
@@ -5,25 +5,51 @@ import java.util.Set;
 
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.data.ShapeFeature;
+import processing.core.PGraphics;
 
-public class Airport implements Comparable<Airport> {
-	PointFeature feature;
+public class Airport extends PointFeature implements Comparable<Airport> {
+	private static final int WHITE = 255;
+	private AirportMarker marker;
 
 	@Override
 	public String toString() {
-		return String.format("Airport %s, %s [routes=%d, Id=%s]", feature.getProperty("name"), 
-				feature.getProperty("city"), routes.size(), feature.getId());
+		return String.format("Airport %s, %s [routes=%d, Id=%s]", getProperty("name"), getProperty("city"), routes.size(), 
+				getId());
 	}
 
-	public Airport(PointFeature feature) {
-		this.feature = feature;
+	public Airport(PointFeature feature, AirportMarker m) {
+		super(feature.location);
+		properties = feature.properties;
+		setId(feature.getId());
+		setMarker(m);
 		routes = new HashSet<ShapeFeature>();
 	}
 
 	Set<ShapeFeature> routes;
-
+	private static int maxRoutes = 0;
 	@Override
 	public int compareTo(Airport o) {
 		return routes.size() - o.routes.size();
 	}
+
+	public void draw(PGraphics pg, float x, float y) {
+		// Save previous drawing style
+		pg.pushStyle();
+		maxRoutes = Math.max(routes.size(), maxRoutes);
+		int shade = WHITE*routes.size()/maxRoutes;
+		pg.fill(shade);
+		float radius = routes.size();
+		pg.ellipse(location.x, location.y, radius, radius);
+		// Restore previous drawing style
+		pg.popStyle();
+	}
+
+	public AirportMarker getMarker() {
+		return marker;
+	}
+
+	public void setMarker(AirportMarker marker) {
+		this.marker = marker;
+	}
+
 }
--- UCSDUnfoldingMaps/src/module6/AirportMap.java
+++ UCSDUnfoldingMaps/src/module6/AirportMap.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
@@ -27,92 +28,78 @@ import processing.core.PApplet;
  *
  */
 public class AirportMap extends PApplet {
-	
+
 	UnfoldingMap map;
 	private List<Marker> airportList;
 	List<Marker> routeList;
-	Map<Integer, Airport> airports;
+	private Iterator<Airport> airportIterator;
 	
 	public void setup() {
 		// setting up PAppler
 		size(800,600, OPENGL);
-		
+
 		// setting up map and default events
 		map = new UnfoldingMap(this, 50, 50, 750, 550);
 		MapUtils.createDefaultEventDispatcher(this, map);
-		
+
 		// get features from airport data
 		List<PointFeature> features = ParseFeed.parseAirports(this, "airports.dat");
-		
+
 		// list for markers, hashmap for quicker access when matching with routes
 		airportList = new ArrayList<Marker>();
-		airports = new HashMap<Integer, Airport>(features.size());
+		HashMap<Integer, Airport> airports = new HashMap<Integer, Airport>(features.size());
 		HashMap<Integer, Location> locations = new HashMap<Integer, Location>();
-		
+
 		// create markers from features
 		for(PointFeature feature : features) {
 			AirportMarker m = new AirportMarker(feature);
-	
+
 			m.setRadius(5);
-			airportList.add(m);
-			
+
 			// put airport in hashmap with OpenFlights unique id for key
 			locations.put(Integer.parseInt(feature.getId()), feature.getLocation());
-			airports.put(Integer.parseInt(feature.getId()), new Airport(feature));
+			airports.put(Integer.parseInt(feature.getId()), new Airport(feature, m));
 		}
-		
-		
+
+
 		// parse route data
 		List<ShapeFeature> routes = ParseFeed.parseRoutes(this, "routes.dat");
 		routeList = new ArrayList<Marker>();
 		for(ShapeFeature route : routes) {
-			
+
 			// get source and destination airportIds
 			int source = Integer.parseInt((String)route.getProperty("source"));
 			int dest = Integer.parseInt((String)route.getProperty("destination"));
-			
+
 			// get locations for airports on route
 			if(locations.containsKey(source) && locations.containsKey(dest)) {
 				route.addLocation(locations.get(source));
 				route.addLocation(locations.get(dest));
 				airports.get(source).routes.add(route);
 				airports.get(dest).routes.add(route);
-//				System.out.println(source + " to " + dest);
 			}
 		}
 		
-		// Add top ten airports to routeList
-		// TODO: clumsy picture, make it interactive
-		SortedSet<Airport> aports = new TreeSet<Airport>(Comparator.reverseOrder());
-		aports.addAll(airports.values());
-		int topPorts = 10;
-		for(Airport port : aports) {
-			System.out.println(port);
-			if(topPorts-- <= 0)
-				break;
-			int routeCount = 10;
-			for(ShapeFeature route : port.routes) {
-				if(routeCount-- <= 0)
-					break;	
-				SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
-				System.out.println(sl.getProperties());
-				routeList.add(sl);
-			}
-		}		
-		
-		
-		//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
-		map.addMarkers(routeList);
-		
-//		map.addMarkers(airportList);
-		
+		// Prepare for mouse clicks
+		airportIterator = airports.values().iterator();
 	}
 	
+	@Override
+	public void mouseClicked() {
+		Airport airport = null;
+		if(airportIterator.hasNext()) {
+			airport = airportIterator.next();
+			airportList.add(airport.getMarker());
+		}
+		System.out.println(airport);
+		map.addMarkers(airportList);
+	}
+
 	public void draw() {
 		background(0);
 		map.draw();
-		
+
 	}
-	
+
 
 }
